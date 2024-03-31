e98b6a67885571b9b38c1b3efc309c7eb8627aaf
GouDZZA <146658676+GouDZZA@users.noreply.github.com>
2024-03-31 11:33:51 +0300

    Creating project

---
+++ src/main/java/by/likebebras/testprojectt/TestProjectt.java
@@ -0,0 +1,17 @@
+package by.likebebras.testprojectt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TestProjectt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
