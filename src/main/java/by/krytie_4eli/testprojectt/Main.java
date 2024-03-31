5fb1fc63e3d3692e0e94b0acc7117a82c3924091
GouDZZA <146658676+GouDZZA@users.noreply.github.com>
2024-03-31 12:05:33 +0300

    fix x3 blin

--- src/main/java/by/krytie_4eli/testprojectt/Main.java
+++ src/main/java/by/krytie_4eli/testprojectt/Main.java
@@ -4,15 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
-    private static Main instace;
+    private static Main instance;
 
-    public static Main getInstace() {
-        return instace;
+    public static Main getInstance() {
+        return instance;
     }
 
     @Override
     public void onEnable() {
-        instace = this;
+        instance = this;
     }
 
     @Override
